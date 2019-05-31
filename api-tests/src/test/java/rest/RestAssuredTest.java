8fd97384f5c9afabb15c56c4bbdced45cf17d0c0
Michal Krzyzanowski <mich.krzyzanowski@gmail.com>
2019-05-31 02:31:05 +0200

    API tests: update JUnit, Gradle, RestAssured

--- api-tests/src/test/java/rest/RestAssuredTest.java
+++ api-tests/src/test/java/rest/RestAssuredTest.java
@@ -1,12 +1,12 @@
 package rest;
 
-import com.jayway.restassured.RestAssured;
-import org.junit.Test;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-import static com.jayway.restassured.RestAssured.given;
-import static com.jayway.restassured.RestAssured.when;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 
